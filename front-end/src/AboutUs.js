520d55f90c44fe846716ede8a11eeb3da60c640a
r8btx <60275558+r8btx@users.noreply.github.com>
2023-09-28 02:54:38 -0400

    moved aboutus to backend

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,46 +1,60 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './AboutUs.css'
-import myself from './myself.jpg'
+import loadingIcon from './loading.gif'
 
 /**
- * A React component that is used for the header displayed at the top of every page of the site.
+ * A React component that shows the about us page.
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
-
 const AboutUs = props => {
+  const [content, setContent] = useState({ img: '', text: [] })
+  const [loaded, setLoaded] = useState(false)
+  const [error, setError] = useState('')
+
+  /**
+   * A nested function that fetches content from the back-end server.
+   */
+  const fetchContent = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/aboutus/content`)
+      .then(response => {
+        // axios bundles up all response data in response.data property
+        setContent({
+          img: `${process.env.REACT_APP_SERVER_HOSTNAME}${response.data.image}`,
+          text: response.data.text,
+        })
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+      .finally(() => {
+        // the response has been received, so remove the loading icon
+        setLoaded(true)
+      })
+  }
+
+  // set up loading data from server when the component first loads
+  useEffect(() => {
+    // fetch content once
+    fetchContent()
+  }, []) // putting a blank array as second argument will cause this function to run only once when component first loads
+
   return (
-    <div className="aboutus">
-      <img src={myself} alt="myself" width={'300px'} />
-      <p>
-        Hi, I am r8btx! <br></br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Enim tortor at
-        auctor urna nunc id cursus metus. Scelerisque mauris pellentesque
-        pulvinar pellentesque habitant morbi. Ac odio tempor orci dapibus
-        ultrices in. Pretium nibh ipsum consequat nisl vel. Felis eget nunc
-        lobortis mattis aliquam faucibus purus. Eget dolor morbi non arcu.
-        Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc
-        sed. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare
-        massa eget. Eu scelerisque felis imperdiet proin fermentum. Est
-        ullamcorper eget nulla facilisi etiam dignissim diam.
-      </p>
-      <p>
-        Amet risus nullam eget felis. Pellentesque pulvinar pellentesque
-        habitant morbi tristique senectus et. Diam quam nulla porttitor massa id
-        neque aliquam vestibulum. Quis ipsum suspendisse ultrices gravida dictum
-        fusce. Eget nunc scelerisque viverra mauris in aliquam. Praesent semper
-        feugiat nibh sed. Euismod lacinia at quis risus. Adipiscing diam donec
-        adipiscing tristique. Semper risus in hendrerit gravida rutrum quisque
-        non tellus orci. Nunc lobortis mattis aliquam faucibus purus in massa
-        tempor. Risus feugiat in ante metus. Quis enim lobortis scelerisque
-        fermentum dui. Nisi scelerisque eu ultrices vitae auctor eu augue ut
-        lectus. Nunc id cursus metus aliquam. Turpis nunc eget lorem dolor sed
-        viverra. Libero id faucibus nisl tincidunt eget. Dui faucibus in ornare
-        quam viverra. Ut diam quam nulla porttitor. Risus quis varius quam
-        quisque id diam vel. Consectetur purus ut faucibus pulvinar elementum
-        integer enim.
-      </p>
-    </div>
+    <>
+      <h1>About Us</h1>
+
+      <div className="aboutus">
+        {error && <p className="aboutUs-error">{error}</p>}
+        {!loaded && <img src={loadingIcon} alt="loading" />}
+
+        {loaded && <img src={content.img} alt="myself" />}
+        {content.text.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
+    </>
   )
 }
 
